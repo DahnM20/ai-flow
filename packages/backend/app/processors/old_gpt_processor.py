6835b35e7fb26fe02d80a6e2df91bfd6b4f70fb3
Dahn <134054382+DahnM20@users.noreply.github.com>
2023-07-08 19:50:59 +0200

    Fix light theme

--- packages/backend/app/processors/old_gpt_processor.py
+++ packages/backend/app/processors/old_gpt_processor.py
@@ -1,32 +1,37 @@
 from .processor import Processor
 
+
 class OldGPTProcessor(Processor):
     processor_type = "gpt"
-    
+
     def __init__(self, config):
         super().__init__(config)
-        self.init_data = config.get("initData") if config.get("initData")  else ""
-            
+        self.init_data = config.get("initData") if config.get("initData") else ""
+
         self.model = config.get("gptVersion")
         self.messages = [
             {"role": "system", "content": self.init_data},
         ]
-        
+
     def createInitialContent(self):
-        if hasattr(self, 'input_processor') and self.input_processor is not None:
+        if hasattr(self, "input_processor") and self.input_processor is not None:
             input_processor_output = self.input_processor.get_output(self.input_key)
             print("input_processor_output ")
             print(input_processor_output)
-            contextSuffix =  " To answer my next request, you will use the following data : " +  input_processor_output if input_processor_output  else ""
+            contextSuffix = (
+                " To answer my next request, you will use the following data : "
+                + input_processor_output
+                if input_processor_output
+                else ""
+            )
         else:
             contextSuffix = ""
-            
-        
+
         return self.init_data + contextSuffix
-    
+
     def updateContext(self, data):
         self.messages.append(data)
-        
+
     def process(self):
         self.messages.append(
             {
@@ -34,8 +39,6 @@ class OldGPTProcessor(Processor):
                 "content": self.createInitialContent(),
             }
         )
-        self.messages.append(
-            {"role": "assistant", "content": "Very well."}
-        )
+        self.messages.append({"role": "assistant", "content": "Very well."})
         self.set_output(self.messages)
         return self.messages
