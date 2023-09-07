b0fc0f6bc7c71ce5e14ebe0aaac24798cd7cf9e8
Dahn <134054382+DahnM20@users.noreply.github.com>
2023-09-07 16:22:09 +0200

    fix: issue when loading processors after having replaced one

--- packages/backend/app/processors_utils/processor_launcher.py
+++ packages/backend/app/processors_utils/processor_launcher.py
@@ -8,6 +8,7 @@ from .processor_factory import ProcessorFactory
 
 store = ProcessorStoreSingleton().store
 
+
 def load_config_data(fileName):
     # Lecture du fichier de configuration
     with open(fileName, "r") as file:
@@ -21,6 +22,9 @@ def link_processors(processors):
             input_name = processor.input
             input_processor = processors.get(input_name)
             if not input_processor:
+                logging.error(
+                    f"Link_processors - processor name : '{processor.name}' - input_processor : ''{processor.input}'"
+                )
                 raise ValueError(f"Input processor '{input_name}' not found")
             processor.set_input_processor(input_processor)
 
@@ -45,11 +49,36 @@ def load_new_processors_and_stored_ones(config_data, stored_processors, node_nam
     factory = ProcessorFactory()
     factory.load_processors()
 
-    logging.debug("Stored processors: ",stored_processors)
+    config_names = [config["name"] for config in config_data]
+
+    if stored_processors is not None:
+        new_stored_processors = {}
+        for key, value in stored_processors.items():
+            processor_name = key.split("_")[1]
+            processor_input = getattr(value, "input", None)
+
+            logging.debug(
+                f"Processing key: {key}, name: {processor_name}, input: {processor_input}"
+            )
+
+            if processor_name in config_names:
+                if processor_input is None or processor_input in config_names:
+                    new_stored_processors[key] = value
+                else:
+                    logging.debug(f"Filtering out processor due to input: {key}")
+            else:
+                logging.debug(f"Filtering out processor due to name: {key}")
+        stored_processors = new_stored_processors
+
+    logging.debug("Final stored processors: ", stored_processors)
     processors = {}
     for config in config_data:
         processor_key = f"{session_id}_{config['name']}"
-        if stored_processors is None or processor_key not in stored_processors or config["name"] == node_name:
+        if (
+            stored_processors is None
+            or processor_key not in stored_processors
+            or config["name"] == node_name
+        ):
             processor = factory.create_processor(config)
             processors[config["name"]] = processor
         else:
@@ -61,7 +90,9 @@ def load_processors_for_node(config_data, stored_processors, node_name):
     factory = ProcessorFactory()
     factory.load_processors()
 
-    processors = load_new_processors_and_stored_ones(config_data, stored_processors, node_name)
+    processors = load_new_processors_and_stored_ones(
+        config_data, stored_processors, node_name
+    )
 
     # Établissement des liens entre les processeurs
     link_processors(processors)
@@ -73,15 +104,14 @@ def launchProcessors(processors, ws=False):
     for processor in processors.values():
         if ws:
             emit("current_node_running", {"instance_name": processor.name})
-        
+
         output = processor.process()
         logging.debug(processor.name, "-", processor.processor_type, ": ", output)
-        
+
         # Store processor for future runs
         processor_key = f"{session_id}_{processor.name}"
         store.set(processor_key, processor)
-         
-        
+
         if ws:
             emit("progress", {"instance_name": processor.name, "output": output})
 
@@ -90,18 +120,18 @@ def launch_processors_for_node(processors, node_name=None, ws=False):
     session_id = request.sid
     for processor in processors.values():
         processor_key = f"{session_id}_{processor.name}"
-        
+
         if processor.get_output() is None or processor.name == node_name:
             if ws:
                 emit("current_node_running", {"instance_name": processor.name})
-                
+
             output = processor.process()
             logging.debug(processor.name, "-", processor.processor_type, ": ", output)
-            
+
             # Store processor for future runs
             processor_key = f"{session_id}_{processor.name}"
             store.set(processor_key, processor)
-            
+
             if ws:
                 emit("progress", {"instance_name": processor.name, "output": output})
         if processor.name == node_name:
