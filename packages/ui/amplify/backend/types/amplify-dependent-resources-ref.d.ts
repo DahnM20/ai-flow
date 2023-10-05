94c810866956811acef9f99e005b17a03ac019f7
Dahn <134054382+DahnM20@users.noreply.github.com>
2023-10-05 17:58:33 +0200

    feat: init amplify

---
+++ packages/ui/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "aiflowfa7513ae": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
