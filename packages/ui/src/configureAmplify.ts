cfc039c3ecb13d9ef96b72237f7678f462d2909a
Dahn <134054382+DahnM20@users.noreply.github.com>
2023-10-22 18:41:43 +0200

    fix: amplify configuration issue

--- packages/ui/src/configureAmplify.ts
+++ packages/ui/src/configureAmplify.ts
@@ -3,16 +3,17 @@ import { Amplify } from "aws-amplify";
 const configureAmplify = async () => {
     const useAuth = process.env.REACT_APP_USE_AUTH || 'false';
 
-    if (useAuth.toLowerCase() === 'true') {
-        // @ts-ignore
-        import('./utils/aws-exports')
-            .then((awsExports) => {
-                Amplify.configure(awsExports.default);
-            })
-            .catch((error) => {
-                console.error("Erreur lors de l'importation de aws-exports:", error);
-            });
+    let awsExports;
+    try {
+        awsExports = require('./aws-exports');
+    } catch (err) {
+        console.error("aws-exports.js not found");
     }
+
+    if (awsExports && useAuth === 'true') {
+        Amplify.configure(awsExports);
+    }
+
 };
 
 export default configureAmplify;
