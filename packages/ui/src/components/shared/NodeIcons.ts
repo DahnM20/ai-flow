849caf466d7e7842b82c37f0bd34e96f65970fce
Dahn <134054382+DahnM20@users.noreply.github.com>
2023-11-07 07:26:39 +0100

    feat: update nodeIcons

--- packages/ui/src/components/shared/NodeIcons.ts
+++ packages/ui/src/components/shared/NodeIcons.ts
@@ -1,6 +1,6 @@
 import { FC } from "react";
 import { AiOutlineMergeCells } from "react-icons/ai";
-import { FaUserCircle, FaRobot, FaPlay, FaLink, FaFilm, FaImage } from 'react-icons/fa';
+import { FaUserCircle, FaRobot, FaPlay, FaLink, FaFilm, FaImage, FaEye } from 'react-icons/fa';
 
 export const ICON_MAP: { [key: string]: FC } = {
   'FaUserCircle': FaUserCircle,
@@ -9,5 +9,6 @@ export const ICON_MAP: { [key: string]: FC } = {
   "FaLink": FaLink,
   "FaFilm": FaFilm,
   "FaImage": FaImage,
+  "FaEye": FaEye,
   "AiOutlineMergeCells": AiOutlineMergeCells,
 };
