f95c6ec7ac01d0772d0f24ba866895823931a9c8
Dahn <134054382+DahnM20@users.noreply.github.com>
2023-11-30 10:29:59 +0100

    feat: update url_input_processor to use the new mecanism

--- packages/backend/app/processors/types/url_input_processor.py
+++ packages/backend/app/processors/types/url_input_processor.py
@@ -1,9 +1,13 @@
+from queue import Empty, Queue
+import time
+import eventlet
 from .processor import SimpleProcessor
-from langchain.document_loaders.url import UnstructuredURLLoader
-from langchain.document_loaders import PlaywrightURLLoader
+
+from ...tasks.shared_ressources import scrapping_task_queue
 
 
 class URLInputProcessor(SimpleProcessor):
+    WAIT_TIMEOUT = 60
     processor_type = "url_input"
 
     def __init__(self, config):
@@ -15,10 +19,24 @@ class URLInputProcessor(SimpleProcessor):
 
     def process(self):
         urls = [self.url]
-        # loader = UnstructuredURLLoader(urls)
-        loader = PlaywrightURLLoader(urls=urls, remove_selectors=["header", "footer"])
-
-        documents = loader.load()
-        content = " ".join(doc.page_content for doc in documents)
+        results_queue = Queue()
+        
+        content = None
+        scrapping_task_queue.put((results_queue, urls))
+        
+        start_time = time.time()
+        
+        while True:
+            try:
+                content = results_queue.get_nowait()
+                break
+            except Empty:
+                if time.time() - start_time > URLInputProcessor.WAIT_TIMEOUT :
+                    content = "Timeout"
+                    break
+            
+            eventlet.sleep(0.1)  # Sleep to prevent high CPU usage
+        
+        
         self.set_output(content)
         return content
