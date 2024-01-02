d9ae94215ed8dd75fffc59e117f7c0345cb633a0
DahnM20 <134054382+DahnM20@users.noreply.github.com>
2024-01-02 21:25:56 +0100

    feat: add hightlighted models

--- packages/backend/app/flask/app_routes/node_routes.py
+++ packages/backend/app/flask/app_routes/node_routes.py
@@ -1,10 +1,10 @@
-import logging
 from flask import Blueprint, request
 from ...utils.replicate_utils import (
     get_model_openapi_schema,
     get_replicate_models,
     get_replicate_collections,
     get_replicate_collection_models,
+    get_highlighted_models_info,
 )
 
 
@@ -14,7 +14,11 @@ node_blueprint = Blueprint("node_blueprint", __name__)
 @node_blueprint.route("/node/models")
 def get_public_models():
     cursor = request.args.get("cursor", None)
-    return get_replicate_models(cursor=cursor)
+
+    public = get_replicate_models(cursor=cursor)
+    highlighted = get_highlighted_models_info()
+
+    return {"public": public, "highlighted": highlighted}
 
 
 @node_blueprint.route("/node/collections")
