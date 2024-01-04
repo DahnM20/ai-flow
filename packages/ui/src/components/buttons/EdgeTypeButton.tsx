f3091cad0c58851a8f6827cca4a085d662441a15
DahnM20 <134054382+DahnM20@users.noreply.github.com>
2024-01-04 08:07:16 +0100

    feat: improve EdgeTypeButton

--- packages/ui/src/components/buttons/EdgeTypeButton.tsx
+++ packages/ui/src/components/buttons/EdgeTypeButton.tsx
@@ -1,4 +1,6 @@
 import { useMemo } from "react";
+import { useTranslation } from "react-i18next";
+import { Tooltip } from "react-tooltip";
 
 export interface EdgeTypeIconEntry {
     src: string;
@@ -11,6 +13,7 @@ interface EdgeTypeButtonProps {
 }
 
 export default function EdgeTypeButton({ edgeType, onChangeEdgeType }: EdgeTypeButtonProps) {
+    const { t } = useTranslation('flow');
 
     const edgeTypeIconsMapping = useMemo<EdgeTypeIconEntry[]>(() => [
         { src: `./curve-edge.svg`, edgeType: 'default' },
@@ -31,8 +34,11 @@ export default function EdgeTypeButton({ edgeType, onChangeEdgeType }: EdgeTypeB
 
     return (
         <div className='ring-slate-400/30 ring-1 rounded-lg items-center'
-            onClick={handleChangeEdgeType}>
+            onClick={handleChangeEdgeType}
+            data-tooltip-id={`edge-type-button-tooltip`}
+            data-tooltip-content={t("EdgeType")}>
             <img className="h-full w-full" src={currentEdgeType?.src} alt={currentEdgeType?.edgeType} />
+            <Tooltip id={`edge-type-button-tooltip`} style={{ zIndex: 100 }} />
         </div>
     )
 
