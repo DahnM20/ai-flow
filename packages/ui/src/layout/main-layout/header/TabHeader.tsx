6cc94d08a91439c3625ed9d9f0c276bee2ece0ff
DahnM20 <134054382+DahnM20@users.noreply.github.com>
2024-02-04 14:44:52 +0100

    chore: change logo file

--- packages/ui/src/layout/main-layout/header/TabHeader.tsx
+++ packages/ui/src/layout/main-layout/header/TabHeader.tsx
@@ -1,47 +1,55 @@
-import React from 'react';
-import { FaPlus } from 'react-icons/fa';
-import styled from 'styled-components';
-import Tab from './Tab';
-
+import React from "react";
+import { FaPlus } from "react-icons/fa";
+import styled from "styled-components";
+import Tab from "./Tab";
 
 interface TabHeaderProps {
-    tabs: any[];
-    currentTab: number;
-    onChangeTab: (index: number) => void;
-    onDeleteTab: (index: number) => void;
-    onAddFlowTab: () => void;
-    tabPrefix: string;
-    children: React.ReactNode;
+  tabs: any[];
+  currentTab: number;
+  onChangeTab: (index: number) => void;
+  onDeleteTab: (index: number) => void;
+  onAddFlowTab: () => void;
+  tabPrefix: string;
+  children: React.ReactNode;
 }
 
-const TabHeader = ({ tabs, currentTab, onChangeTab, onDeleteTab, onAddFlowTab, tabPrefix, children }: TabHeaderProps) => {
-    return (
-        <TabsContainer className='flex flex-row items-center justify-center h-16 py-2 border-b-sky-950/50 z-30'>
-            <div className='ml-4 mx-auto flex flex-row text-center align-middle justify-center'>
-                <img src="logo.png" className='w-16' alt="Logo"></img>
-            </div>
-            <Tabs>
-                {tabs.map((tab: any, index: number) => (
-                    <Tab
-                        key={index}
-                        index={index}
-                        active={index === currentTab}
-                        onChangeTab={onChangeTab}
-                        onDeleteTab={onDeleteTab}>
-                        {tabPrefix} {index + 1}
-                    </Tab>
-                ))}
-            </Tabs>
-            <AddTabButton onClick={onAddFlowTab} className='text-lg text-slate-200 hover:text-slate-50 hover:ring-2 ring-slate-200 rounded-lg py-1 px-1'>
-                <FaPlus />
-            </AddTabButton>
-            {
-                children
-            }
-        </TabsContainer>
-    );
+const TabHeader = ({
+  tabs,
+  currentTab,
+  onChangeTab,
+  onDeleteTab,
+  onAddFlowTab,
+  tabPrefix,
+  children,
+}: TabHeaderProps) => {
+  return (
+    <TabsContainer className="z-30 flex h-16 flex-row items-center justify-center border-b-sky-950/50 py-2">
+      <div className="mx-auto ml-4 flex w-10 flex-row justify-center pr-2 text-center align-middle">
+        <img src="logo.svg" alt="Logo"></img>
+      </div>
+      <Tabs>
+        {tabs.map((tab: any, index: number) => (
+          <Tab
+            key={index}
+            index={index}
+            active={index === currentTab}
+            onChangeTab={onChangeTab}
+            onDeleteTab={onDeleteTab}
+          >
+            {tabPrefix} {index + 1}
+          </Tab>
+        ))}
+      </Tabs>
+      <AddTabButton
+        onClick={onAddFlowTab}
+        className="rounded-lg px-1 py-1 text-lg text-slate-200 ring-slate-200 hover:text-slate-50 hover:ring-2"
+      >
+        <FaPlus />
+      </AddTabButton>
+      {children}
+    </TabsContainer>
+  );
 };
-
 
 const TabsContainer = styled.div`
   font-family: Roboto;
@@ -55,7 +63,6 @@ const Tabs = styled.div`
   max-width: 60%;
 `;
 
-const AddTabButton = styled.div`
-`;
+const AddTabButton = styled.div``;
 
 export default TabHeader;
