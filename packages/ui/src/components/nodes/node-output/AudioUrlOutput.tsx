0e539d2a31d9e0bcbe548e06cab6bfbba1c72b18
DahnM20 <134054382+DahnM20@users.noreply.github.com>
2024-04-06 18:36:33 +0200

    feat: update audio player

--- packages/ui/src/components/nodes/node-output/AudioUrlOutput.tsx
+++ packages/ui/src/components/nodes/node-output/AudioUrlOutput.tsx
@@ -1,33 +1,39 @@
-import React from 'react';
-import { FaDownload } from 'react-icons/fa';
-import styled from 'styled-components';
-import { getGeneratedFileName } from './outputUtils';
+import React from "react";
+import { FaDownload } from "react-icons/fa";
+import styled from "styled-components";
+import { getGeneratedFileName } from "./outputUtils";
+import AudioPlayer from "react-h5-audio-player";
+import "react-h5-audio-player/lib/styles.css";
 
 interface AudioUrlOutputProps {
-    url: string;
-    name: string;
+  url: string;
+  name: string;
 }
 
 const AudioUrlOutput: React.FC<AudioUrlOutputProps> = ({ url, name }) => {
+  const handleDownloadClick = (event: React.MouseEvent) => {
+    event.stopPropagation();
+    const link = document.createElement("a");
+    link.href = url;
+    link.download = getGeneratedFileName(url, name);
+    link.target = "_blank";
+    link.click();
+  };
 
-    const handleDownloadClick = (event: React.MouseEvent) => {
-        event.stopPropagation();
-        const link = document.createElement('a');
-        link.href = url;
-        link.download = getGeneratedFileName(url, name);
-        link.target = '_blank';
-        link.click();
-    };
-
-    return <OutputAudioContainer>
-        <OutputAudio controls src={url} />
-        <div
-            className='absolute top-2 right-3 px-1 py-1 text-slate-100 text-2xl bg-slate-600/75 hover:bg-sky-600/90 rounded-md'
-            onClick={handleDownloadClick}>
-            <FaDownload />
-        </div>
+  return (
+    <OutputAudioContainer className="audio-player w-full pt-12">
+      <StyledAudioPlayer src={url} />
+      <div
+        className="absolute right-3 top-2 rounded-md bg-slate-600/75 px-1 py-1 text-2xl text-slate-100 hover:bg-sky-600/90"
+        onClick={handleDownloadClick}
+      >
+        <FaDownload />
+      </div>
     </OutputAudioContainer>
+  );
 };
+
+const StyledAudioPlayer = styled(AudioPlayer)``;
 
 const OutputAudioContainer = styled.div`
   display: flex;
@@ -35,11 +41,6 @@ const OutputAudioContainer = styled.div`
   align-items: center;
   position: relative;
   margin-top: 10px;
-`;
-
-const OutputAudio = styled.audio`
-  min-height: 100px;
-  width: 100%;
 `;
 
 export default AudioUrlOutput;
