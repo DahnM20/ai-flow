f7e6b87744a92b1f2238d1320bc9e1e1e4426f84
DahnM20 <134054382+DahnM20@users.noreply.github.com>
2024-04-27 12:21:48 +0200

    feat: white and blacklist mecanism for extensions

--- packages/backend/app/utils/node_extension_utils.py
+++ packages/backend/app/utils/node_extension_utils.py
@@ -1,4 +1,5 @@
 import importlib
+import os
 import pkgutil
 from cachetools import TTLCache, cached
 
@@ -8,8 +9,14 @@ from ..processors.components.extension.extension_base_processor import (
 
 very_long_ttl_cache = 120000
 
+raw_blacklist = os.getenv("EXTENSION_BLACKLIST", "").strip()
+EXTENSION_BLACKLIST = raw_blacklist.split(",") if raw_blacklist else []
 
-def _load_all_schemas():
+raw_whitelist = os.getenv("EXTENSION_WHITELIST", "").strip()
+EXTENSION_WHITELIST = raw_whitelist.split(",") if raw_whitelist else []
+
+
+def _load_all_extension_schemas():
     schemas = []
     package = importlib.import_module("app.processors.components.extension")
     prefix = package.__name__ + "."
@@ -24,13 +31,25 @@ def _load_all_schemas():
             ):
                 if hasattr(attribute, "get_schema"):
                     schema = attribute.get_schema(attribute)
-                    schemas.append(schema)
+                    if schema is not None:
+                        schemas.append(schema)
     return schemas
+
+
+def filter_extensions(extensions):
+    if len(EXTENSION_WHITELIST) > 0:
+        extensions = [e for e in extensions if e.processorType in EXTENSION_WHITELIST]
+    if len(EXTENSION_BLACKLIST) > 0:
+        extensions = [
+            e for e in extensions if e.processorType not in EXTENSION_BLACKLIST
+        ]
+    return extensions
 
 
 @cached(TTLCache(maxsize=100, ttl=very_long_ttl_cache))
 def get_extensions():
-    schemas = _load_all_schemas()
+    schemas = _load_all_extension_schemas()
+    schemas = filter_extensions(schemas)
     schemas_dict = []
     for schema in schemas:
         if schema is not None:
