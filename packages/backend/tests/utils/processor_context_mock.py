86c35c6e2666314524c60f565e0d6ead070abc85
DahnM20 <134054382+DahnM20@users.noreply.github.com>
2024-04-28 06:59:26 +0200

    fix: update mock for unit tests

--- packages/backend/tests/utils/processor_context_mock.py
+++ packages/backend/tests/utils/processor_context_mock.py
@@ -1,11 +1,12 @@
 from app.processors.context.processor_context import ProcessorContext
+from typing import List
 
 
 class ProcessorContextMock(ProcessorContext):
     def __init__(self, api_key, user_id=0, session_id=0) -> None:
         super().__init__()
         self.api_key = api_key
-        self.user_id= user_id
+        self.user_id = user_id
         self.session_id = session_id
 
     def get_context(self):
@@ -16,9 +17,15 @@ class ProcessorContextMock(ProcessorContext):
 
     def get_api_key_for_provider(self, provider):
         return self.api_key
-    
-    def  get_current_user_id(self):
+
+    def get_current_user_id(self):
         return self.user_id
-    
-    def  get_session_id(self):
+
+    def get_session_id(self):
         return self.user_id
+
+    def get_parameter_names(self) -> List[str]:
+        return super().get_parameter_names()
+
+    def get_value(self, name):
+        return super().get_value(name)
