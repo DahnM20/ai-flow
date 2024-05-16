73acc3bf2e4ac6d7aedb1dd785eaa9bdc25fa9d3
DahnM20 <134054382+DahnM20@users.noreply.github.com>
2024-05-16 14:31:07 +0200

    fix: Dockerfile and config

--- packages/ui/src/config/config.ts
+++ packages/ui/src/config/config.ts
@@ -1,6 +1,6 @@
 const HOST = import.meta.env.VITE_APP_WS_HOST || "localhost";
 const WS_PORT = import.meta.env.VITE_APP_WS_PORT || 5000;
-const REST_API_PORT = import.meta.env.VITE_APP_REST_API_PORT || 5000;
+const REST_API_PORT = import.meta.env.VITE_APP_API_REST_PORT || 5000;
 const USE_HTTPS = import.meta.env.VITE_APP_USE_HTTPS || "false";
 const USE_CACHE = import.meta.env.VITE_APP_USE_CACHE?.toLowerCase() || "true";
 const CURRENT_APP_VERSION = import.meta.env.VITE_APP_VERSION;
