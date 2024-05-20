12546a718aa634687752669e6644d1b32996fad6
DahnM20 <134054382+DahnM20@users.noreply.github.com>
2024-05-20 14:48:03 +0200

    fix: popup grid

--- packages/ui/src/components/popups/shared/Grid.tsx
+++ packages/ui/src/components/popups/shared/Grid.tsx
@@ -2,32 +2,41 @@ import styled from "styled-components";
 
 interface GridProps<T> {
   items: T[];
-  onValidate: (item: T) => boolean;
-  renderItem: (item: T, onValidate: (item: T) => boolean) => JSX.Element;
+  renderItem: (item: T, onValidate: (id: string) => void) => JSX.Element;
+  onValidate: (id: string) => void;
   numberColMax?: number;
 }
 
 const getGridTemplateColumns = (maxCols: number) => {
   return `
     grid-template-columns: repeat(1, minmax(0, 1fr));
-    
     @media (min-width: 640px) {
       grid-template-columns: repeat(${Math.min(2, maxCols)}, minmax(0, 1fr));
     }
-    
     @media (min-width: 768px) {
       grid-template-columns: repeat(${Math.min(3, maxCols)}, minmax(0, 1fr));
     }
-    
     @media (min-width: 1024px) {
       grid-template-columns: repeat(${Math.min(4, maxCols)}, minmax(0, 1fr));
     }
-    
     @media (min-width: 1280px) {
       grid-template-columns: repeat(${maxCols}, minmax(0, 1fr));
     }
   `;
 };
+
+export default function Grid<T>({
+  items,
+  onValidate,
+  renderItem,
+  numberColMax = 2,
+}: GridProps<T>) {
+  return (
+    <StyledGrid maxCols={numberColMax}>
+      {items && items.map((item) => renderItem(item, onValidate))}
+    </StyledGrid>
+  );
+}
 
 const StyledGrid = styled.div<{ maxCols: number }>`
   display: grid;
@@ -35,17 +44,3 @@ const StyledGrid = styled.div<{ maxCols: number }>`
   gap: 1rem;
   ${({ maxCols }) => getGridTemplateColumns(maxCols)}
 `;
-
-export default function Grid<T>({
-  items,
-  onValidate,
-  renderItem,
-  numberColMax = 5,
-  ...props
-}: GridProps<T>) {
-  return (
-    <StyledGrid maxCols={numberColMax} {...props}>
-      {items && items.map((item) => renderItem(item, onValidate))}
-    </StyledGrid>
-  );
-}
