05a4707e7ec47c96cb5b630d5e14a229df53781d
DahnM20 <134054382+DahnM20@users.noreply.github.com>
2024-05-20 14:35:59 +0200

    fix: column display in replicate popup

--- packages/ui/src/components/popups/shared/Grid.tsx
+++ packages/ui/src/components/popups/shared/Grid.tsx
@@ -1,9 +1,40 @@
+import styled from "styled-components";
+
 interface GridProps<T> {
   items: T[];
-  renderItem: (item: T, onValidate: (id: string) => void) => JSX.Element;
-  onValidate: (id: string) => void;
+  onValidate: (item: T) => boolean;
+  renderItem: (item: T, onValidate: (item: T) => boolean) => JSX.Element;
   numberColMax?: number;
 }
+
+const getGridTemplateColumns = (maxCols: number) => {
+  return `
+    grid-template-columns: repeat(1, minmax(0, 1fr));
+    
+    @media (min-width: 640px) {
+      grid-template-columns: repeat(${Math.min(2, maxCols)}, minmax(0, 1fr));
+    }
+    
+    @media (min-width: 768px) {
+      grid-template-columns: repeat(${Math.min(3, maxCols)}, minmax(0, 1fr));
+    }
+    
+    @media (min-width: 1024px) {
+      grid-template-columns: repeat(${Math.min(4, maxCols)}, minmax(0, 1fr));
+    }
+    
+    @media (min-width: 1280px) {
+      grid-template-columns: repeat(${maxCols}, minmax(0, 1fr));
+    }
+  `;
+};
+
+const StyledGrid = styled.div<{ maxCols: number }>`
+  display: grid;
+  width: 100%;
+  gap: 1rem;
+  ${({ maxCols }) => getGridTemplateColumns(maxCols)}
+`;
 
 export default function Grid<T>({
   items,
@@ -12,19 +43,9 @@ export default function Grid<T>({
   numberColMax = 5,
   ...props
 }: GridProps<T>) {
-  const getGridColsClass = (maxCols: number) => {
-    const colsClasses = [
-      `grid-cols-1`,
-      `sm:grid-cols-${Math.min(2, maxCols)}`,
-      `md:grid-cols-${Math.min(3, maxCols)}`,
-      `lg:grid-cols-${Math.min(4, maxCols)}`,
-      `xl:grid-cols-${maxCols}`,
-    ];
-    return colsClasses.join(" ");
-  };
   return (
-    <div className={`grid w-full gap-4 ${getGridColsClass(numberColMax)}`}>
+    <StyledGrid maxCols={numberColMax} {...props}>
       {items && items.map((item) => renderItem(item, onValidate))}
-    </div>
+    </StyledGrid>
   );
 }
