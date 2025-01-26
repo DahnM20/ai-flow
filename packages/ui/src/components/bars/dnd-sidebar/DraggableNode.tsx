65070f128d0ba33fa9ec911d253ac2ae291c0f7b
DahnM20 <134054382+DahnM20@users.noreply.github.com>
2025-01-26 08:17:55 +0100

    feat: update DraggableNode appearance

--- packages/ui/src/components/bars/dnd-sidebar/DraggableNode.tsx
+++ packages/ui/src/components/bars/dnd-sidebar/DraggableNode.tsx
@@ -5,9 +5,15 @@ import { ReactNode, memo } from "react";
 import styled from "styled-components";
 import { toastCustomIconInfoMessage } from "../../../utils/toastUtils";
 import { FiMenu, FiMove } from "react-icons/fi";
+import { darken, lighten } from "polished";
+import { Tooltip } from "@mantine/core";
+import { GripIcon } from "./GripIcon";
 
 interface DraggableNodeProps {
   node: DnDNode;
+  id?: string;
+  additionnalNodeData?: any;
+  additionnalNodeConfig?: any;
 }
 
 interface NodeBadgeProps {
@@ -24,7 +30,11 @@ const DraggableNode = (props: DraggableNodeProps) => {
 
   const [{ isDragging }, drag] = useDrag({
     type: "NODE",
-    item: { nodeType: props.node.type },
+    item: {
+      nodeType: props.node.type,
+      additionnalData: props.additionnalNodeData,
+      additionnalConfig: props.additionnalNodeConfig,
+    },
     collect: (monitor) => {
       const result = {
         isDragging: monitor.isDragging(),
@@ -45,19 +55,24 @@ const DraggableNode = (props: DraggableNodeProps) => {
   }
 
   return (
-    <Node
-      ref={drag}
-      id={props.node.type}
-      onClick={(e) => {
-        e.stopPropagation();
-      }}
-      onTouchEnd={(e) => {
-        e.stopPropagation();
-      }}
-      onDoubleClick={(e) => {
-        showDragAndDropHelper();
-      }}
-      className={`sidebar-dnd-node text-md group group relative flex h-auto 
+    <Tooltip
+      label={t(props.node.helpMessage ?? "")}
+      color="gray"
+      openDelay={300}
+    >
+      <Node
+        ref={drag}
+        id={props.id ?? props.node.type}
+        onClick={(e) => {
+          e.stopPropagation();
+        }}
+        onTouchEnd={(e) => {
+          e.stopPropagation();
+        }}
+        onDoubleClick={(e) => {
+          showDragAndDropHelper();
+        }}
+        className={`sidebar-dnd-node text-md group group relative flex h-auto 
                   w-full
                   cursor-grab
                   flex-row items-center justify-between
@@ -67,22 +82,28 @@ const DraggableNode = (props: DraggableNodeProps) => {
                   transition-all duration-200 ease-in-out 
                   hover:ring-2 hover:ring-slate-200/10 
                   ${isDragging ? "opacity-10" : ""}`}
-      data-tooltip-id={`dnd-tooltip`}
-      data-tooltip-content={t(props.node.helpMessage ?? "")}
-    >
-      <div className="flex w-full flex-row items-center justify-between space-x-1 px-2 text-center">
-        <p className="flex-grow">{t(props.node.label)}</p>
-        <FiMenu className="text-slate-400/60 transition-colors duration-75 ease-in-out group-hover:text-slate-200" />
-      </div>
+      >
+        <div className="flex w-full flex-row items-center justify-between space-x-1 px-2 text-center">
+          <p className="flex-grow truncate ">{t(props.node.label)}</p>
+          <GripIcon className="h-4 w-4 text-slate-400/60 transition-colors duration-75 ease-in-out group-hover:text-slate-200/60" />
+        </div>
 
-      {props.node.isBeta && <NodeBadge>Beta</NodeBadge>}
-    </Node>
+        {props.node.isBeta && <NodeBadge>Beta</NodeBadge>}
+      </Node>
+    </Tooltip>
   );
 };
 
-const Node = styled.div`
+const Node = styled.div<{ bandColor?: string }>`
   background:
-    linear-gradient(120deg, #84fab0 0%, #8fd3f4 100%) left / 2% no-repeat,
+    linear-gradient(
+        120deg,
+        ${({ bandColor }) => (bandColor ? lighten(0.05, bandColor) : "#84fab0")}
+          0%,
+        ${({ bandColor }) => (bandColor ? darken(0.1, bandColor) : "#8fd3f4")}
+          100%
+      )
+      left / 2% no-repeat,
     ${({ theme }) => theme.bg};
   user-select: none;
   -webkit-user-select: none;
